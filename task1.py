567524dd5e7ef682a8a420535aa0be36703dd20e
Егорова Лилиана Сергеевна <l.egorova@main.nsu.ru>
2025-02-15 07:57:57 +0300

    dhyhe

--- task1.py
+++ task1.py
@@ -5,13 +5,13 @@
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
 with open('task1.txt', encoding='utf-8') as f:
-s=f.read()
-print(len(s))
-worlds = s.split(' ')
-points = s.count('.')
-dash = s.count('—')
-g = len(worlds)
-j = g - points - dash
-strings = s.count('\n')
-print(j)
-print(strings + 1)
+    s=f.read()
+    print(len(s))
+    worlds = s.split(' ')
+    points = s.count('.')
+    dash = s.count('—')
+    g = len(worlds)
+    j = g - points - dash
+    strings = s.count('\n')
+    print(j)
+    print(strings + 1)
--- task5.py
+++ task5.py
@@ -5,3 +5,20 @@
 # Его длина: длина
 #
 # Выведите это слово и длину в консоль.
+
+with open('task5.txt', encoding='utf-8') as f:
+    s = f.read()
+    # print(len(s))
+    w = s.split(' ')
+    max = 0
+    for i in w:
+        d = len(i)
+        if d > max:
+            max = d
+            slovo = i
+
+file = open('newfile.txt', 'w')
+file.write(slovo, max)
+file.close()
+print(slovo)
+print(max)
